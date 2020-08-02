ea32809e6050acdeae93f6e56ea55b899fce2b12
Jesus Diaz <jesus740@gmail.com>
2020-08-02 00:35:26 -0500

    Included config for travis

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+docker build -t jdiaz740/eureka-server:latest -t jdiaz740/eureka-server:$TAG -f ./eureka-server/Dockerfile ./eureka-server
+docker build -t jdiaz740/config-server:latest -t jdiaz740/config-server:$TAG --build-arg ENC_PASS=${JASYPT_PASS} -f ./config-server/Dockerfile ./config-server
+docker build -t jdiaz740/auth-server:latest -t jdiaz740/auth-server:$TAG -f ./auth-server/Dockerfile ./auth-server
+docker build -t jdiaz740/zuul-server:latest -t jdiaz740/zuul-server:$TAG -f ./zuul-server/Dockerfile ./zuul-server
+docker build -t jdiaz740/users-service:latest -t jdiaz740/users-service:$TAG -f ./users-service/Dockerfile ./users-servicedocker buid -t jdiaz740/eureka-server -f ./eureka-server/Dockerfile ./eureka-server
+
+docker push jdiaz740/config-server:latest
+docker push jdiaz740/auth-server:latest
+docker push jdiaz740/zuul-server:latest
+docker push jdiaz740/users-service:latest
+
+docker push jdiaz740/config-server:$TAG
+docker push jdiaz740/auth-server:$TAG
+docker push jdiaz740/zuul-server:$TAG
+docker push jdiaz740/users-service:$TAG
+
+kubectl apply -f k8s
+
+kubectl set image deployments/auth-server-deployment auth-server=jdiaz740/auth-server:$TAG
+kubectl set image deployments/config-server-deployment config-server=jdiaz740/config-server:$TAG
+kubectl set image deployments/eureka-server-deployment eureka-server=jdiaz740/eureka-server:$TAG
+kubectl set image deployments/zuul-server-deployment server-server=jdiaz740/server-server:$TAG
+kubectl set image deployments/users-service-deployment users-service=jdiaz740/users-service:$TAG
